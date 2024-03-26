927c248438f3069b3afd4e8801f7594ce754d7c0
Dylan Spyer <dylanspyer@gmail.com>
2024-03-26 12:04:16 -0500

    uibuild

--- index.js
+++ index.js
@@ -4,29 +4,6 @@ const app = express();
 const mongoose = require("mongoose");
 const Note = require("./models/note");
 
-const password = process.argv[2];
-
-// DO NOT SAVE YOUR PASSWORD TO GITHUB!!
-const url = `mongodb+srv://dylanspyer:${password}@cluster0.sxy7z94.mongodb.net/noteApp?retryWrites=true&w=majority`;
-
-mongoose.set("strictQuery", false);
-mongoose.connect(url);
-
-const noteSchema = new mongoose.Schema({
-  content: String,
-  important: Boolean,
-});
-
-noteSchema.set("toJSON", {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
-  },
-});
-
-const Note = mongoose.model("Note", noteSchema);
-
 let notes = [
   // {
   //   id: 1,
@@ -80,40 +57,50 @@ app.get("/api/notes", (request, response) => {
   });
 });
 
-const generateId = () => {
-  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
-  return maxId + 1;
-};
+// const generateId = () => {
+//   const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
+//   return maxId + 1;
+// };
 
 app.post("/api/notes", (request, response) => {
   const body = request.body;
 
-  if (!body.content) {
-    return response.status(400).json({
-      error: "content missing",
-    });
+  if (body.content === undefined) {
+    return response.status(400).json({ error: "content missing" });
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
-    id: generateId(),
-  };
+  });
 
-  notes = notes.concat(note);
+  note.save().then((savedNote) => {
+    response.json(savedNote);
+  });
 
-  response.json(note);
+  // const note = {
+  //   content: body.content,
+  //   important: body.important || false,
+  //   id: generateId(),
+  // };
+
+  // notes = notes.concat(note);
+
+  // response.json(note);
 });
 
 app.get("/api/notes/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const note = notes.find((note) => note.id === id);
-  if (note) {
+  Note.findById(request.params.id).then((note) => {
     response.json(note);
-  } else {
-    console.log("x");
-    response.status(404).end();
-  }
+  });
+  // const id = Number(request.params.id);
+  // const note = notes.find((note) => note.id === id);
+  // if (note) {
+  //   response.json(note);
+  // } else {
+  //   console.log("x");
+  //   response.status(404).end();
+  // }
 });
 
 app.delete("/api/notes/:id", (request, response) => {
